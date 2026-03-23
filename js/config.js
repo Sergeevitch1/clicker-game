8b0c880453520f751cca965df4b57c79e3e07d0b
Sergeevitch1 <166446866+Sergeevitch1@users.noreply.github.com>
2026-03-23 11:48:09 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,22 @@
+// ===== FIREBASE CONFIG =====
+const firebaseConfig = {
+    apiKey: "AIzaSyAhsAZIQPeudjXsO275v9wf1BuVM0x8B1g",
+    authDomain: "clicker-game-22323.firebaseapp.com",
+    databaseURL: "https://clicker-game-22323-default-rtdb.firebaseio.com",
+    projectId: "кликер-игра-22323",
+    storageBucket: "clicker-game-22323.firebasestorage.app",
+    messagingSenderId: "851035040900",
+    appId: "1:851035040900:web:2cea7586861cb4deee26d1",
+    measurementId: "G-D71LKP8962"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+// Telegram WebApp
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+// Get user ID
+const userId = tg.initDataUnsafe?.user?.id || 'guest';
